aeb9aa4d222f00153ffa53ddfaefbf769cbdaa28
Safnas <iamsafnas@gmail.com>
2020-04-10 21:08:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,176 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+//const FacebookStrategy = require('passport-facebook').Strategy
+const findOrCreate = require('mongoose-findorcreate')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect('mongodb://localhost:27017/secretDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set("useCreateIndex", true)
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    //facebookId: String
+})
+
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
+
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy())
+
+// passport.serializeUser(User.serializeUser())
+// passport.deserializeUser(User.deserializeUser())
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user)
+    })
+})
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/secrets'
+},
+    (accessToken, refreshToken, profile, cb) => {
+        //console.log(profile)
+        User.findOrCreate({ googleId: profile.id }, (err, user) => {
+            return cb(err, user)
+        })
+    }
+))
+
+// passport.use(new FacebookStrategy({
+//     clientID: process.env.FACEBOOK_APP_ID,
+//     clientSecret: process.env.FACEBOOK_APP_SECRET,
+//     callbackURL: "http://localhost:3000/auth/facebook/secrets",
+//     profileFields: ["email", "name"]
+// },
+//     function (accessToken, refreshToken, profile, cb) {
+//         console.log(profile._json)
+//         const { email, first_name, last_name } = profile._json;
+//         console.log(email)
+//         User.findOrCreate({ facebookId: email }, (err, user) => {
+//             return cb(err, user);
+//         });
+//     }
+// ));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }))
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/secrets');
+    });
+
+// app.get('/auth/facebook',
+//     passport.authenticate('facebook', { scope: ['id'] }))
+
+// app.get('/auth/facebook/secrets',
+//     passport.authenticate('facebook', { failureRedirect: '/login' }),
+//     function (req, res) {
+//         // Successful authentication, redirect home.
+//         res.redirect('/secrets');
+//     });
+
+
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', (req, res) => {
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err)
+            res.redirect('/register')
+        } else {
+            passport.authenticate('local')(
+                req, res, () => {
+                    res.redirect('/secrets')
+                }
+            )
+        }
+    })
+})
+
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) {
+        //console.log(req.user)
+        res.render('secrets')
+    } else {
+        res.redirect('/login')
+    }
+})
+
+app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+app.post('/login', (req, res) => {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            passport.authenticate('local')(
+                req, res, () => {
+                    res.redirect('/secrets')
+                }
+            )
+        }
+    })
+
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+})
